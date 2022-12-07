7f1e9848890cf0e04022c41c5b943cf2e16cf230
MANMITH GOPARI <119970011+Manmith7@users.noreply.github.com>
2022-12-07 22:46:04 +0530

    Add files via upload

---
+++ projectmanmith.py
@@ -0,0 +1,20 @@
+n = int(input("Enter Number: "))
+a = int(input())
+b = int(input())
+count = 0
+if n in range(a, b):
+    if n % 2 == 0:
+        print(n, "is a Even Number")
+    else:
+        print(n, "is a odd number")
+    if n > 0:
+        print(n, "is a positive number")
+    else:
+        print(n, "is a Negitive Number")
+    for i in range(2, n):
+        if n % i == 0:
+            count = count + 1
+    if count == 0:
+        print(n, "is a Prime Number")
+    else:
+        print(n, "is a Composite Number")
